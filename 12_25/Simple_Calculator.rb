0e536a7fd2a78cc1756d8d778b22158e7faa46be
book_mima <pingpongken1@gmail.com>
2017-12-25 15:05:36 +0900

    ans3を追加

--- 12_25/Simple_Calculator.rb
+++ 12_25/Simple_Calculator.rb
@@ -1,20 +1,21 @@
-# while true  do
-#   arr = gets.split
-#   num1 = arr[0].to_i
-#   num2 = arr[2].to_i
-#   operator = arr[1]
-#   if operator == "+"
-#     puts num1 + num2
-#   elsif operator == "-"
-#     puts num1 - num2
-#   elsif operator == "*"
-#     puts num1 * num2
-#   elsif operator == "/"
-#     puts num1 / num2 
-#   else
-#     break
-#   end
-# end
+#! ruby -n
+while true  do
+  arr = gets.split
+  num1 = arr[0].to_i
+  num2 = arr[2].to_i
+  operator = arr[1]
+  if operator == "+"
+    puts num1 + num2
+  elsif operator == "-"
+    puts num1 - num2
+  elsif operator == "*"
+    puts num1 * num2
+  elsif operator == "/"
+    puts num1 / num2 
+  else
+    break
+  end
+end
 
 # ans1
 # loop{
@@ -36,8 +37,12 @@
 # }
 
 # ans2
-loop{
-  a,op,b = gets.split
-  break if op == "?"
-  puts a.to_i.send(op,b.to_i)
-}
+# loop{
+#   a,op,b = gets.split
+#   break if op == "?"
+#   puts a.to_i.send(op,b.to_i)
+# }
+
+# ans3
+# exit if $_[??] 
+# puts eval($_)
