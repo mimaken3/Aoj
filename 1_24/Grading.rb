852116e74d06b27efc31ecb5f3456b63ffdffbe8
book_mima <ken0930mata@ezweb.ne.jp>
2018-01-24 12:04:23 +0900

    first commit

---
+++ 1_24/Grading.rb
@@ -0,0 +1,26 @@
+loop {
+  sum = 0
+  m_score,f_score,r_score = gets.split.map(&:to_i)
+  break if m_score == -1 && f_score == -1 && r_score == -1
+  sum = m_score + f_score
+
+  if m_score == -1 || f_score == -1
+    puts "F" 
+  else
+    if sum >= 80
+      puts "A"
+    elsif sum < 80 && sum >= 65
+      puts "B"
+    elsif sum < 65 && sum >= 50
+      puts "C"
+    elsif sum < 50 && sum >= 30
+      if r_score >= 50
+        puts "C"
+      else
+        puts "D"
+      end
+    else 
+      puts "F"
+    end
+  end
+}
