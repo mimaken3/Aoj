d3c36ee3699917ad3ad1f6b28a2560c36b743045
book_mima <ken0930mata@ezweb.ne.jp>
2018-01-26 14:02:47 +0900

    Test Case2をクリア

--- 1_26/How_Many_Ways.rb
+++ 1_26/How_Many_Ways.rb
@@ -2,12 +2,11 @@ while (n,x = gets.split.map(&:to_i)) != [0,0] do
   count = 0
   check_arr = Array.new
   test_arr  = Array.new
-  check_num = 0
   c = 0
   (1..n).each {|i|
     (1..n).each {|j|
       (1..n).each {|k|
-        if (i + j + k) == 9 && i != j && i != k && j != k
+        if (i + j + k) == x && i != j && i != k && j != k
           test_arr.clear
           test_arr = i,j,k
           if c == 0 
@@ -16,13 +15,17 @@ while (n,x = gets.split.map(&:to_i)) != [0,0] do
             # puts "if c == 0の中"
             count += 1
           else
+            check_num = 0
             (0..(c - 1)).each {|c|
+              # puts "eachの中"
               if check_arr[c].sort == test_arr.sort  
+                # puts "breakしまーす"
                 break
               end
+              # puts "check_numを１増やしまーす"
               check_num += 1
             }
-            if check_num == c - 1
+            if check_num == c 
                 # print check_arr[c].sort
                 # puts
                 # print test_arr.sort
