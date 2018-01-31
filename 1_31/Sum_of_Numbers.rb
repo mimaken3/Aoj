91b30dad229f3497ce4ac3ee05b3a21a12d9cbc2
book_mima <ken0930mata@ezweb.ne.jp>
2018-01-31 10:56:33 +0900

    回答例を２つ追加

--- 1_31/Sum_of_Numbers.rb
+++ 1_31/Sum_of_Numbers.rb
@@ -1,6 +1,4 @@
 while (line = gets.chomp) != "0" do
-# loop {
-#   line = gets.chomp
   break if line == 0
   sum = 0
   line.each_char {|st|
@@ -8,4 +6,16 @@ while (line = gets.chomp) != "0" do
   }
   puts sum
 end
-  # }
+
+#1
+# while (line = gets.chomp) != "0" 
+#   puts line.chars.map(&:to_i).reduce(:+)
+# end
+
+# 2
+# while (line = gets.chomp) != "0"
+  # inject{|sum,n| sum + n}と一緒
+  # ちなみに初期値を指定した場合
+  # inject(100,:+)
+#   puts line.chars.map(&:to_i).inject(10,:+)
+# end
