8131f864a67ac630953347637eb25e026e6fd157
book_mima <ken0930mata@ezweb.ne.jp>
2018-01-31 11:33:12 +0900

    回答例を追加

--- 1_31/Counting_Characters.rb
+++ 1_31/Counting_Characters.rb
@@ -1,18 +1,34 @@
-str = gets.chomp
-table = Hash.new(0)
+# str = ""
+# while s = gets.chomp
+#   str << s
+# end
+#
+# str.downcase!
+#
+# table = Hash.new(0)
+#
+# str.scan(/./){|m| table[m] += 1}
+# # p table
+# ("a".."z").each {|al|
+#   num = 0
+#
+# table.each{|key, value|
+#   if key == al
+#     num = value
+#   end
+# }
+#
+# puts "#{al} : #{num}"
+# }
 
-str.scan(/./){|m| table[m] += 1}
-# p table
-("a".."z").each {|al|
-  num = 0
-
-table.each{|key, value|
-  if key == al
-    num = value
-  elsif key == al.upcase
-    num = value
-  end
-}
-
-puts "#{al} : #{num}"
-}
+str = "";
+ 
+while s = gets do
+  str << s.chomp;
+end
+ 
+str.downcase!;
+ 
+for c in 'a'..'z' do
+  puts c + " : " + str.count(c).to_s;
+end
