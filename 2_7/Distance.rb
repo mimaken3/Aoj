8354eb5af406fe8a613d73b6ce2fc820c2fae76f
book_mima <ken0930mata@ezweb.ne.jp>
2018-02-07 16:59:27 +0900

    回答例を追加

--- 2_7/Distance.rb
+++ 2_7/Distance.rb
@@ -2,3 +2,9 @@ x1,y1, x2,y2 = gets.split.map(&:to_f)
 
 distance = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
 puts distance.to_f
+
+#1
+# x1, y1, x2, y2 = gets.split.map(&:to_f)
+# puts Math.hypot(x1 - x2, y1 - y2)
+# hypot(x, y) は
+# sqrt(x*x + y*y) を返します。
