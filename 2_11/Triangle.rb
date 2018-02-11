82dfe97ecf9cc3646e9cdb7165d896f9729cc305
book_mima <ken0930mata@ezweb.ne.jp>
2018-02-11 14:55:00 +0900

    回答例を追加

--- 2_11/Triangle.rb
+++ 2_11/Triangle.rb
@@ -7,3 +7,10 @@ s = (a + b + d)/2
 puts area = Math.sqrt(s*(s-a)*(s-b)*(s-d))
 puts length = a + b + d
 
+#1
+# include Math
+# a, b, c = gets.split.map(&:to_f)
+# h = sin(c *= PI / 180) * b
+# puts a / 2 * h
+# puts a + b + sqrt(a * a + b * b - 2 * a * b * cos(c))
+# puts h
