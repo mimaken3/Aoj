ebcd1fb048ee0bb8e35c866df412c834f1cbb655
book_mima <ken0930mata@ezweb.ne.jp>
2018-02-12 10:40:06 +0900

    回答例を追加

--- 2_12/Standard_Deviation.rb
+++ 2_12/Standard_Deviation.rb
@@ -7,7 +7,13 @@ ave = line.inject{|sum,i| sum += i}/n
 sum = 0
 line.each { |n|
 sum += ((n - ave) ** 2)
-# puts sum
 }
 puts sta_dev = sqrt(sum / n)
 end
+
+#1
+# while (n = gets.to_i) != 0
+#   line = gets.split.map(&:to_f)
+#   ave = line.reduce(:+) / n
+#   puts Math.sqrt(line.map{|x| (x - ave) ** 2}.reduce(:+) / n)
+# end
