94f2b307f68b63b1586bb8f8d7b0879cb641da78
book_mima <ken0930mata@ezweb.ne.jp>
2018-02-14 20:34:55 +0900

    ２つめの回答例をコメントアウト

--- 2_14/Distance_2.rb
+++ 2_14/Distance_2.rb
@@ -1,3 +1,4 @@
+# 1
 gets
 x = gets.split(" ").map(&:to_f)
 y = gets.split(" ").map(&:to_f)
@@ -6,9 +7,10 @@ d = [x,y].transpose.map{|c| c.inject(:-).abs}
   puts ((i+1 == 4) ? d.max : d.map{|c| c**(i+1)}.inject(:+)**(1.0/(i+1))).round(6)
 end
 
-gets
-a = []
-2.times { a << gets.split.map(&:to_f) }
-a = a.transpose.map { |m, n| (m - n).abs }
-1.upto(3) { |i| p a.map { |e| e ** i }.inject(:+) ** (1.0 / i) }
-p a.max
+# 2
+# gets
+# a = []
+# 2.times { a << gets.split.map(&:to_f) }
+# a = a.transpose.map { |m, n| (m - n).abs }
+# 1.upto(3) { |i| p a.map { |e| e ** i }.inject(:+) ** (1.0 / i) }
+# p a.max
